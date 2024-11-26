f0d2c62fbd5c2ea363dc9c4a9f4d95f7220e8900
GustavoTunes <gustavosouzatunes@gmail.com>
2024-11-26 18:32:37 -0300

    Fix assets dependencies

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
-import "jquery";
-import "bootstrap";
-import "./sb-admin-2.min";
-import "@fortawesome/fontawesome-free";
+import "jquery"
+import "bootstrap"
+import "@fortawesome/fontawesome-free"
 import "chart.js"
-import '../vendor/startbootstrap-sb-admin-2/css/sb-admin-2.min.css';
-import 'startbootstrap-sb-admin-2/js/sb-admin-2.min.js'
+import "startbootstrap-sb-admin-2"
+import "startbootstrap-sb-admin-2-css"
+import "jquery-easing"
--- config/importmap.rb
+++ config/importmap.rb
@@ -6,8 +6,11 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-pin "jquery" # @3.7.1
-pin "bootstrap" # @5.3.3
-pin "@fortawesome/fontawesome-free", to: "@fortawesome--fontawesome-free.js" # @6.7.1
-pin "chart.js" # @4.4.6
-pin "@kurkle/color", to: "@kurkle--color.js" # @0.3.4
+pin "jquery", to: "https://code.jquery.com/jquery-3.7.1.min.js"
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+pin "@fortawesome/fontawesome-free", to: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/js/all.min.js"
+pin "chart.js", to: "https://cdn.jsdelivr.net/npm/chart.js@4.4.6/dist/chart.min.js"
+pin "@kurkle/color", to: "https://cdn.jsdelivr.net/npm/@kurkle/color@0.3.4/dist/color.min.js"
+pin "startbootstrap-sb-admin-2", to: "https://cdn.jsdelivr.net/npm/startbootstrap-sb-admin-2@4.0.2/dist/js/sb-admin-2.min.js"
+pin "startbootstrap-sb-admin-2-css", to: "https://cdn.jsdelivr.net/npm/startbootstrap-sb-admin-2@4.0.2/dist/css/sb-admin-2.min.css"
+pin "jquery-easing", to: "https://cdn.jsdelivr.net/npm/jquery.easing@3.1.1/jquery.easing.min.js"
