537156b5d3320377f5b68c55b0110475f9ec4231
GustavoTunes <gustavosouzatunes@gmail.com>
2024-12-02 14:19:15 -0300

    Refactor project to use CDN

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import "jquery"
-import "bootstrap"
-import "@fortawesome/fontawesome-free"
-import "chart.js"
-import "startbootstrap-sb-admin-2"
-import "jquery-easing"
 
+// Remover essas importações, pois estão no CDN
+// import "jquery"
+// import "bootstrap"
+// import "@fortawesome/fontawesome-free"
+// import "chart.js"
+// import "startbootstrap-sb-admin-2"
+// import "jquery-easing"
 
+// Se você tiver outros arquivos JavaScript customizados, pode importá-los aqui
+import "sb-admin-2-js" // ou outros arquivos customizados que você tenha criado
--- config/importmap.rb
+++ config/importmap.rb
@@ -6,11 +6,9 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
 # Bibliotecas externas (via CDN)
-pin "jquery", to: "https://code.jquery.com/jquery-3.7.1.min.js"
 pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
 pin "@fortawesome/fontawesome-free", to: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/js/all.min.js"
 pin "chart.js", to: "https://cdn.jsdelivr.net/npm/chart.js@4.4.6/dist/chart.min.js"
 pin "startbootstrap-sb-admin-2", to: "https://cdn.jsdelivr.net/npm/startbootstrap-sb-admin-2@4.0.2/dist/js/sb-admin-2.min.js"
 pin "startbootstrap-sb-admin-2-css", to: "https://cdn.jsdelivr.net/npm/startbootstrap-sb-admin-2@4.0.2/dist/css/sb-admin-2.min.css"
 pin "jquery-easing", to: "https://cdn.jsdelivr.net/npm/jquery.easing@3.1.1/jquery.easing.min.js"
-
