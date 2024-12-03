78ac6831ade03c7fbf199b9183781aabd9bd237f
GustavoTunes <gustavosouzatunes@gmail.com>
2024-12-02 22:28:32 -0300

    Fix dependencies versions compatibility

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,9 +1,3 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
-
-//= link startbootstrap-sb-admin-2/js/sb-admin-2.min.js
-//= link startbootstrap-sb-admin-2/css/sb-admin-2.min.css
-//= link @fortawesome/fontawesome-free/css/all.min.css
-//= link jquery.easing/jquery.easing.min.js
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,14 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
 
 // Remover essas importações, pois estão no CDN
-// import "jquery"
-// import "bootstrap"
-// import "@fortawesome/fontawesome-free"
-// import "chart.js"
-// import "startbootstrap-sb-admin-2"
-// import "jquery-easing"
-
-// Se você tiver outros arquivos JavaScript customizados, pode importá-los aqui
-import "sb-admin-2-js" // ou outros arquivos customizados que você tenha criado
+import "jquery";
+import "jquery-easing";
+import "bootstrap";
+import "fontawesome";
+import "chart";
+import "startbootstrap-sb-admin-2"
