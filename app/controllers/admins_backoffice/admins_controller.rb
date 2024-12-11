7ecd52d8aea0ae420799c6f81fdd225ac0784c31
GustavoTunes <gustavosouzatunes@gmail.com>
2024-12-11 15:32:23 -0300

    Add create new admin function

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -6,6 +6,19 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
     @admins = Admin.all
   end
 
+  def new
+    @admin = Admin.new
+  end
+
+  def create
+    @admin = Admin.new(params_admin)
+    if @admin.save
+      redirect_to admins_backoffice_admins_path, notice:"Administrator sucessfully created!"
+    else
+      render :new, status: :unprocessable_entity
+    end     
+  end
+
   def edit
   end
 
