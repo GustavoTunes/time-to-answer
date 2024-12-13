a726ec11dbcde2eef6a8ac606ea0f8247352e435
GustavoTunes <gustavosouzatunes@gmail.com>
2024-12-13 14:35:32 -0300

    Add subjects view page

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   namespace :admins_backoffice do
     get "welcome/index"
     resources :admins
-    resoruces :subjects
+    resources :subjects
   end
   
   devise_for :admins
