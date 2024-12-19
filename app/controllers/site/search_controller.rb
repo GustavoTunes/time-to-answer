8fd1508e2c10e7e2cd01fdb51f248b84a4b455d7
GustavoTunes <gustavosouzatunes@gmail.com>
2024-12-19 15:00:55 -0300

    Refactor 'site' controllers

--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -1,8 +1,6 @@
 class Site::SearchController < SiteController
   layout 'site'
   def questions
-    @questions = Question.includes(:answers)
-                         .where("lower(description) LIKE ?", "%#{params[:term].downcase}%")
-                         .page(params[:page])
+    @questions = Question.search(params[:page], params[:term])
   end
 end
